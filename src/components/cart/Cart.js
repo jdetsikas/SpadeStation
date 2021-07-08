b411652537d8e9d4ed9ab1a664654a82f38cdd23
John Detsikas <john.o.detsikas@gmail.com>
2021-07-08 12:01:32 -0500

    Restored the old cart that got deleted

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CartGameTemplate from './CartGameTemplate'
+
+const Cart = () => {
+    return (
+        <div id='cart'>
+            <h2>Cart</h2>
+            <h3>List goes here</h3>
+            <button>Checkout</button>
+        </div> );
+}
+
+export default Cart
