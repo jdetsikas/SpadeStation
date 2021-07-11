3de9d267aaa99edd8b673aad607d4cc9a35f35fb
John Detsikas <john.o.detsikas@gmail.com>
2021-07-11 12:15:39 -0500

    Changes index.js in sendfile portion of server index.js to index.html

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ server.use((err, req, res, next) => {
 
 // For any get routes that are not in /api, rely on ReactRouter to handle
 server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.js'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 // 404 Handler
