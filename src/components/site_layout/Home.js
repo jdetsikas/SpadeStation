a5a2feadaca735fd905dcc686e56fa5d27d6d41f
John Detsikas <john.o.detsikas@gmail.com>
2021-07-22 22:04:20 -0500

    Slight cleaning

--- src/components/site_layout/Home.js
+++ src/components/site_layout/Home.js
@@ -1,35 +1,38 @@
 import React, { useState, useEffect } from 'react'
 
 function Home(props) {
-  return (
-    <div className='Home'>
-      <div className='bigwelcome'>
-      <div className='welcoming'>
-      <h1>Welcome to SPADE STATION!</h1>
-      </div>
-      <div className='welcoming2'>
-      <h1>We've Got Games On Deck!</h1>
-      </div>
-      </div>
-      <div id="slideshow">
-      
-  <div className="slide-wrapper">
-    <div className="slide"><div className="slide-img"><img src ="/icons/banner1.jpg" alt="banner1"/></div></div>
-    <div className="slide"><div className="slide-img"><img src ="/icons/banner2.jpg" alt="banner2"/></div></div>
-    <div className="slide"><div className="slide-img"><img src ="/icons/banner3.jpg" alt="banner3"/></div></div>
-    <div className="slide"><div className="slide-img"><img src ="/icons/banner4.jpg" alt="banner4"/></div></div>
-    <div className="slide"><div className="slide-img">5</div></div>
-  </div>
-</div>
+	return (
+		<div className='Home'>
 
-<p className="line1">
-    <img src ="/icons/action.jpg" alt="action"/>
-    <img src ="/icons/rpg.jpg" alt="rpg"/>
-    <img src ="/icons/platformer.jpg" alt="paltformer"/>
-    <img src ="/icons/strat.jpg" alt="strat"/>   
-</p>
-      
-    </div> );
+			<div className='bigwelcome'>
+				<div className='welcoming'>
+					<h1>Welcome to SPADE STATION!</h1>
+				</div>
+
+				<div className='welcoming2'>
+					<h1>We've Got Games On Deck!</h1>
+				</div>
+			</div>
+
+			<div id="slideshow">
+				<div className="slide-wrapper">
+					<div className="slide"><div className="slide-img"><img src ="/icons/banner1.jpg" alt="banner1"/></div></div>
+					<div className="slide"><div className="slide-img"><img src ="/icons/banner2.jpg" alt="banner2"/></div></div>
+					<div className="slide"><div className="slide-img"><img src ="/icons/banner3.jpg" alt="banner3"/></div></div>
+					<div className="slide"><div className="slide-img"><img src ="/icons/banner4.jpg" alt="banner4"/></div></div>
+					<div className="slide"><div className="slide-img">5</div></div>
+				</div>
+			</div>
+
+			<p className="line1">
+				<img src ="/icons/action.jpg" alt="action"/>
+				<img src ="/icons/rpg.jpg" alt="rpg"/>
+				<img src ="/icons/platformer.jpg" alt="paltformer"/>
+				<img src ="/icons/strat.jpg" alt="strat"/>   
+			</p>
+
+		</div> 
+	)
 }
 
 export default Home
